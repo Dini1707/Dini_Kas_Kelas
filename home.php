f06ec9f1e5fd6a283d31646dba71aa382b895ad7
Dini1707 <dinirahmaaprilianti1707@gmail.com>
2023-01-20 13:20:12 +0700

    up

---
+++ home.php
@@ -0,0 +1,57 @@
+<?php
+include "isi.php";
+?>
+
+<div class="sidebar">
+      <div class="sidebar">
+       <nav id="sidebarMenu" class="col-md-2 col-lg-2 d-md-block sidebar collapse" style="color: black">
+
+          <div class="sidebar-sticky pt-3">
+            <ul class="navbar-nav flex-column" style="margin-top:20% ;">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  <img src="assets/Home" class="icon" alt="" />
+                  Home
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="index">home</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="order">
+                  <img src="assets/Trolly" alt="" class="icon" />
+                  Orders
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="report">
+                  <img src="assets/Report" alt="" class="icon" />
+                  Report
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  <img src="assets/Stats" class="icon" alt="" />
+                  Stats
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="status">Chart</a>
+                  <a class="dropdown-item" href="status #Table">Table</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                  <img src="assets/Contact" class="icon" alt="" />
+                  Contact
+                </a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="form">Form</a>
+                  <a class="dropdown-item" href="https://wa.me/6285861880292">Whatsapp</a>
+                  <a class="dropdown-item" href="https://www.instagram.com/diirapril28_/?next=%2F">Instagram</a>
+                </div>
+              </li>
+            </ul>
+
+            
+          </div>
+        </nav>
--- index.php
+++ index.php
@@ -1,34 +1,31 @@
 <?php
-$username ='admin';
-$password ='admin';
+
+include "boot.php";
 ?>
+<?php
+session_start();
+$username = 'admin';
+$password = 'admin';
+if (isset($_POST['submit'])){
+  if ($_POST['username'] == $username && $_POST ['password'] == $password){
+    $_SESSION["username"] == $username;
+    echo "Login Sukses $username";
+  }else{
 
+    login();
+    echo 'Login Gagal';
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-   
+  }
+}else{
+  login();
+}
 
-  </head>
-  <body style=" background-color: #8AB5B2;">
-       <img src="logo.png" alt="" class="logo"><br>
+function login(){ ?>
+<img src="logo.png" alt="" class="logo"><br>
        <p ><h5 class="judul">Harap Input  Username Dan Password</h5></p>
-       <form action="POST">
+       <form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
         <input type="text" class="input1" id="username" placeholder="E-mail/UserName" name="username">
-        <input type="text" class="input2" id="password" placeholder="Password" name="password">
-
+        <input type="password" class="input2" id="password" placeholder="Password" name="password">
+        <input class="button" type="submit" name="submit" value="login">
        </form>
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-       <button type="button" class="button">Login</button>
-  </body>
-</html>
-
-
-
-
+<?php }?>
