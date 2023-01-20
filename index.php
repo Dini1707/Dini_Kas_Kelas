28aec9db8915e835d8e3b951e819c9f4957e607e
Dini1707 <dinirahmaaprilianti1707@gmail.com>
2023-01-20 09:39:54 +0700

    up

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+$username ='admin';
+$password ='admin';
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,19 +11,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   
 
   </head>
   <body style=" background-color: #8AB5B2;">
        <img src="logo.png" alt="" class="logo"><br>
-       <form action="">
-        <input type="text" class="input1">
-        <input type="email" class="input1">
+       <p ><h5 class="judul">Harap Input  Username Dan Password</h5></p>
+       <form action="POST">
+        <input type="text" class="input1" id="username" placeholder="E-mail/UserName" name="username">
+        <input type="text" class="input2" id="password" placeholder="Password" name="password">
 
        </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+       <button type="button" class="button">Login</button>
   </body>
 </html>
 
