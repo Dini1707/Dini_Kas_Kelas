0136478743ea996fdd1c4bec24cec7e4431c15ec
Dini1707 <dinirahmaaprilianti1707@gmail.com>
2023-02-01 13:31:35 +0700

    up

--- admin/home.php
+++ admin/home.php
@@ -5,6 +5,6 @@ include "../koneksi.php";
 ?>
 <link rel="stylesheet" href="../style.css">
 <div class="kotak1">
-    <p>uang masuk</p>
-    <img src="../img/masuk.png" alt="">
+    <p class="">uang masuk</p>
+    <img class="" src="../img/masuk.png" alt="">
 </div>
