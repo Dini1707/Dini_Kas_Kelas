2b53b256dbe3750f205876331454cfec589110a9
Dini1707 <dinirahmaaprilianti1707@gmail.com>
2023-02-01 14:06:07 +0700

    up

--- admin/home.php
+++ admin/home.php
@@ -5,6 +5,16 @@ include "../koneksi.php";
 ?>
 <link rel="stylesheet" href="../style.css">
 <div class="kotak1">
-    <p class="">uang masuk</p>
-    <img class="" src="../img/masuk.png" alt="">
+    <p class="p1">uang masuk</p>
+    <img class="g1" src="../img/masuk.png" alt="">
+</div>
+<link rel="stylesheet" href="../style.css">
+<div class="kotak2">
+    <p class="p1">Uang Keluar</p>
+    <img class="g1" src="../img/masuk.png" alt="">
+</div>
+<link rel="stylesheet" href="../style.css">
+<div class="kotak3">
+    <p class="p1">Total</p>
+    <img class="g1" src="../img/masuk.png" alt="">
 </div>
